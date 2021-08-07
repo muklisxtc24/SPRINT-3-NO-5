43486455f83223d09bebf6793066fc7dd8b68ff7
muklisxtc24 <87222847+muklisxtc24@users.noreply.github.com>
2021-08-07 07:13:44 +0700

    Add files via upload

---
+++ TUGAS1.js
@@ -0,0 +1,21 @@
+let newCallback = (nama,asal) => {
+
+  console.log(
+    `${nama} ${asal}`
+);
+pulang()
+}
+
+let pulang = () => {
+
+console.log(
+    'Pulang Kampung'
+);
+
+}
+
+let nama = 'adi'
+let asal = 'yusuf'
+
+newCallback(nama,asal,pulang)
+
---
+++ tugas3.js
@@ -0,0 +1,2 @@
+//Async/Await itu adalah sebuah syntax khusus yang digunakan untuk
+//menangani Promise agar penulisan code lebih efisien dan rapih
